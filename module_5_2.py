ec2e7b58aafbfea549bb3342a8cf1bbdbecb11a1
NAT-G44 <gr.natasha.2003@gmail.com>
2024-12-04 17:34:43 +0800

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,16 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
+
+my_house = House("ЖК Мечта", 20)
+print(len(my_house))
+print(my_house)
+
+
